849397af369ef1c6dd135bd8719fbb7b0b6d4b70
michael philetus weller <philetus@gmail.com>
2017-09-26 13:53:03 -0700

    cleaned up startup script

--- start_admin.sh
+++ start_admin.sh
@@ -1,7 +1,6 @@
-# create new ponzu project at $GOPATH/project - needs to match $PROJECT_FOLDER
-# echo "initializing ponzu project"
-# ponzu new project
+# script to build & start ponzu in container
 
+# create new ponzu project at $GOPATH/project - needs to match $PROJECT_FOLDER
 echo "creating the volume assets"
 mkdir -p $PONZU_SHARE/uploads
 mkdir -p $PONZU_SHARE/search
@@ -12,24 +11,10 @@ echo "linking shared volume to project folder"
 rm -rf $PROJECT_FOLDER
 ln -sf $PONZU_SHARE $PROJECT_FOLDER
 
-# echo "linking the volume assets"
-# ln -sf $PONZU_SHARE/uploads $PROJECT_FOLDER/uploads
-# ln -sf $PONZU_SHARE/search $PROJECT_FOLDER/search
-# ln -sf $PONZU_SHARE/system.db $PROJECT_FOLDER/system.db
-# ln -sf $PONZU_SHARE/analytics.db $PROJECT_FOLDER/analytics.db
-
 if [ "$1" = "start" ]; then
     echo "building ponzu from project directory"
     cd $PROJECT_FOLDER && ponzu build
 
     echo "starting ponzu admin and api"
     cd $PROJECT_FOLDER && ponzu run --port=8080 --https admin,api &>> $PONZU_SHARE/server.log
-
-    # this line starts and pipes to log, then continues terminal.
-    # cd $PROJECT_FOLDER && nohup ponzu -port=8080 --https run admin,api &> $PONZU_SHARE/server.log &
-    #
-
-    echo "Ponzu server started"
-    echo | pwd
-    sleep infinity
 fi
